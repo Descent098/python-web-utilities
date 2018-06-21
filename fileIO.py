b224aa4121026249d9a2b7c1700fb35bf14546dd
Kieran Wood <kieranw098@gmail.com>
2018-06-21 12:58:04 -0600

    Working State
    Still need to make the write an append so that it can run constantly and not die partway through

--- fileIO.py
+++ fileIO.py
@@ -1,13 +1,29 @@
 def parse_file():
     """Takes the path of a file as input and parses it"""
-    fn = open("redirects_list.txt")
+    fn = open("redirects_list_raw_mini.txt")
     dat = []
     URLS = []
     line = fn.readlines()
-    dat = [url.split("\n") for url in line] #single line list comprehension
+    dat = [url.split("\n") for url in line]  # single line list comprehension
     for li in dat:
         for URL in li:
-            if "https://" in URL :
-                URLS.add(URL)
+            if "https://n" in URL:
+                URLS.append(URL.lstrip())
+                URLS.append("\n")
+            if "Added:" in URL:
+                URLS.append(URL.lstrip())
+                URLS.append("\n")
             else:
-                print("\nURL loop output: {}".format(URL)
+                #print("\nNot a URL loop output: {}".format(URL))
+                None
+    return URLS
+
+
+def save_list(list_data):
+    """Takes in data and writes it to file"""
+    new_filename = "Last_Raw_Data"  # Hard coded for current use
+    new_filename += ".txt"
+    q = open(new_filename, "w")
+    for info in list_data:
+        print(info)
+        q.write(info)
--- redirects.py
+++ redirects.py
@@ -1,25 +1,51 @@
 import requests
-#import fileIO
+from fileIO import *
+
+
 def get_redirects(parsingurl):
     """Traces and prints the redirects of a link provided"""
-    #TODO Make
+    # TODO Make
+
     response = requests.get(parsingurl)
     if response.history:
-        print("Request was redirected")
+        print("\nNew Redirect")
         count = 0
         for resp in response.history:
             strresp = str(resp.url)
-            count+=1
-            info = []
+            count += 1
+            #info = []
             if ("safelinks.protection.outlook.com" in strresp):
-                q =1
-            elif ("safelinks.protection.outlook.com" not in strresp): #parses out safelinks
-                info.append(["Redirect Depth {}".format(count),"\nRedirect type: {}".format(resp.status_code), "\nResponse URL: {}".format(resp.url) + "\n"])
-                print ("Redirect Depth {}".format(count),"\nRedirect type: {}".format(resp.status_code), "\nResponse URL: {}".format(resp.url) + "\n")
-        print("Final (User sees) destination:")
-        print(response.status_code, response.url)
+                None
+            elif ("safelinks.protection.outlook.com" not in strresp):  # parses out safelinks
+                yield ("Redirect Depth {} \nRedirect type: {} \nResponse URL: {}{}".format(count, resp.status_code, resp.url, "\n"))
+                print("Redirect Depth {}".format(count), "\nRedirect type: {}".format(
+                    resp.status_code), "\nResponse URL: {}".format(resp.url) + "\n")
+        yield("Final (User sees) destination: {} \nWith a {} redirect error{}".format(response.url, response.status_code, "\n"))
     else:
-        print ("Request was not redirected")
+        print("Request was not redirected")
 
 
-get_redirects(input("What URL would you like to see the redirect trace for?: "))#Input URL to trace
+# Input URL to trace
+
+
+urls = parse_file()
+
+output = []
+
+for url in urls:
+    try:
+        if "https://" in url:
+            current = url
+            #output.append("\nNew Redirect:\n")
+            for returned in get_redirects(url):
+                output.append(returned)
+                output.append("\n")
+        else:  # for the Added thing
+            output.append(url)
+    except:
+        output.append("Error while checking {}".format(current))
+output = [q for q in output if q is not None]
+output = [q for q in output if q is not ","]
+save_list(output)
+
+# get_redirects(input("What URL would you like to see the redirect trace for?:"))
