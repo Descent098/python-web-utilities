f297725edac157c1e8767374d63badc2014cd2f9
Kieran Wood <kieranw098@gmail.com>
2018-06-29 14:38:41 -0600

    Added un-ordered list parser

--- tag_parsers.py
+++ tag_parsers.py
@@ -4,20 +4,31 @@ import requests
 import bs4
 
 def parse_list(reference, web=False):
-    """Parses ul tags from the URL's html and pulls all the information
+    """Parses all list tags and pulls the information from the ul and li tags
     Returns: String
     Arguments: reference; the path or URL to parse from
     web; True if reference is a URL or false for local filepath"""
-    #TODO implement using local parsing code and pull from URL using requests
-    if web == False:
-        #TODO Implement local path logic
-        None
-    elif web == True:
-        #TODO Implement functionality
-        None
+    #TODO add logic for ordered list parsing
+
+    #TODO Search by class ID
+    #class_id = eval(input("Do you have a class you want to look in? (1)Yes (2)No: "))
+    class_id = 2 #Temp value for testing
+    if class_id == 1:
+        input("What is the class name?: ")
+
+    elif class_id == 2: #if theres not a class to parse
+        if web == False:
+            reference += ".html" #Adding extension to filename
+            html = open(reference, "r")
+
+        elif web == True:
+            html = get_html_from_URL(reference)
+
+        soup = bs4.BeautifulSoup(html, 'html.parser')
+        return soup.find('ul').get_text()
 
 def parse_heading(reference, size="1",web=False):
-    """Parses all h1 tags and pulls the information between the tag
+    """Parses all heading tags and pulls the information between the tag
     Returns: String
     Arguments: reference; the path or URL to parse from
     Size: the heading type i.e. 1 for heading 1
@@ -25,7 +36,7 @@ def parse_heading(reference, size="1",web=False):
 
     #TODO Search by class ID
     #class_id = eval(input("Do you have a class you want to look in? (1)Yes (2)No: "))
-
+    class_id = 2 #Temp value for testing
     if class_id == 1:
         input("What is the class name?: ")
 
@@ -48,21 +59,36 @@ def get_html_from_URL(url):
 def menu_options():
     """Called from main program; compartmentalizing menu options per utility"""
     is_web = eval(input("Is the html from (1)The Web or (2)Local File?: "))
+    if is_web == 1:
+        web = True
+    elif is_web ==2:
+        web = False
     script_choice = eval(input("What would you like to parse? \n(1)Heading Tags \n(2)Lists"))
-    if script_choice == 1:
 
+    if script_choice == 1:
+        None
     elif script_choice ==2:
         #TODO write Lists options
+        None
 
 
 if __name__ == "__main__":
     """Testing stuff; only works if file is run as main"""
 
-    #h1 Parse Testing
+    # #h1 Parse Testing
+    # is_web = eval(input("Is the html from (1)The Web or (2)Local File?: "))
+    # if is_web == 1:
+    #     web = True
+    # elif is_web ==2:
+    #     web = False
+    # #size = input("What size heading are you looking for (i.e. 1 for h1 tags, 2 for h2 tags)?: ")
+    # print(parse_heading(input("File name/url (Excluding Extension i.e. index for index.html): "), "1", web))
+
+    #ul Parse Testing
     is_web = eval(input("Is the html from (1)The Web or (2)Local File?: "))
     if is_web == 1:
         web = True
     elif is_web ==2:
         web = False
     #size = input("What size heading are you looking for (i.e. 1 for h1 tags, 2 for h2 tags)?: ")
-    print(parse_heading(input("File name/url (Excluding Extension i.e. index for index.html): "), "1", web))
+    print(parse_list(input("File name/url (Excluding Extension i.e. index for index.html): "), web))
