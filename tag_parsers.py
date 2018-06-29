ef459a0c64a1f937c7c3831d5f7c163890571899
Kieran Wood <kieranw098@gmail.com>
2018-06-29 12:40:29 -0600

    Ditched LXML (thank god) for BS4

--- tag_parsers.py
+++ tag_parsers.py
@@ -1,6 +1,7 @@
 # Contains Utilities for parsing different tags in html
 from lxml import html
 import requests
+import bs4
 
 def parse_ul_tags(reference, web=False):
     """Parses ul tags from the URL's html and pulls all the information
@@ -20,16 +21,22 @@ def parse_heading_1(reference, web=False):
     Returns: String
     Arguments: reference; the path or URL to parse from
     web; True if reference is a URL or false for local filepath"""
-    if web == False:
-        with open(reference, "r") as f:
-            page = f.read()
-        tree = html.fromstring(page)
-        for i in tree:
-            print(i.drop_tag())
-        print(tree)
+    reference += ".html" #Adding extension to filename
+    class_id = eval(input("Do you have a class you want to look in? (1)Yes (2)No: "))
+    if class_id == 1:
+        input("What is the class name?: ")
 
-    elif web == True:
-        #TODO Implement functionality
-        None
+    elif class_id == 2: #if theres not a class to parse
+        if web == False:
+            html = open(reference, "r")
+            soup = bs4.BeautifulSoup(html, 'html.parser')
+            return soup.find('h1').get_text()
 
-parse_heading_1("ul.html")
+
+        elif web == True:
+            #TODO Implement functionality
+            None
+
+if __name__ == "__main__":
+    """Testing stuff; only works if file is run as main"""
+    print(parse_heading_1(input("File name (Excluding Extension i.e. index for index.html): ")))
