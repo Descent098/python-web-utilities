a652cb7273a5b450f6993f0b89333c5e5c761d36
Kieran Wood <kieranw098@gmail.com>
2018-06-29 15:04:35 -0600

    Finished menu functionality in current state for both scripts

--- program.py
+++ program.py
@@ -1,32 +1,27 @@
-from redirects import *
+from redirects import menu_options as redirects_menu
+from tag_parsers import menu_options as tag_parsers_menu
 from fileIO import *
 import requests
 keep_on = True
 
 print("===================Welcome to Web Utilities===================")
 while keep_on == True:
-    selection = eval(input("What would you like to do? \n(1) Trace a redirect \n(2)Trace redirects from a text file \n(3)exit: "))
+    utility_to_run = (eval(input("What type of utility do you need? \n(1)Redirect tracing (2)HTML Tag parsing: ")))
+    if utility_to_run == 1:
+        redirects_menu()
+    elif utility_to_run == 2:
+        tag_parsers_menu()
+    #TODO Make this level of menu
 
-    if selection == 1:
-        url = input("What is the URL you would like to trace?: ")
-        output = []
-        count = 1
-        try:
-            if ("https://" or "http://") in url:
-                current = url
-                for returned in list(get_redirects(url)):
-                    output.append(returned)
-                    output.append("\n")
-            else:
-                None
-        except:
-            output.append("Error while checking {}".format(current))
-        for i in output:
-            print(i)
-
-    if selection == 2:
-        #TODO fill this out
-        pass
-
-    if selection == 3:
-        keep_on = False
+    # selection = eval(input("What would you like to do? \n(1) Trace a redirect \n(2)Trace redirects from a text file \n(3)exit: "))
+    #
+    # if selection == 1:
+    #     url = input("What is the URL you would like to trace?: ")
+    #     single_URL_trace(url)
+    #
+    # if selection == 2:
+    #     #TODO fill this out
+    #     pass
+    #
+    # if selection == 3:
+    #     keep_on = False
