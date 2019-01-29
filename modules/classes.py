a979f02eb6937009b7cf6d5e1e4bd36b010489dd
Kieran Wood <kieranw098@gmail.com>
2019-01-29 10:41:39 -0700

    Docstring Cleanup

--- modules/classes.py
+++ modules/classes.py
@@ -3,12 +3,22 @@ import requests
 from bs4 import BeautifulSoup
 
 class site:
+    """Class that will contain functions and variables
+    relevent to any sort of parsing"""
     def __init__(self, url):
+        """
+        
+        :type url: String
+        :param url: url for the requested site
+
+
+        """
         self.url = url
         self.soup = BeautifulSoup(requests.get(url))
 
     def __repr__(self):
-        soup.title.string
+        self.soup.title.string
+
 
 
 class validation_spider(scrapy.Spider):
